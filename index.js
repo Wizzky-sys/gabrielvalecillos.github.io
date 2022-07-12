f5fce1dc835ba589e592f4aa4ce0c6b171b4ee30
Gabriel Valecillos <59260137+Wizzky-sys@users.noreply.github.com>
2022-07-12 17:33:29 -0400

    xd

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const clickNav4 = document.querySelector (".contact")
 
 
 
-
+// function location () {
+//   if (location.href === true) {
+//     alert("llegaste a mi cv")
+//   }
+// };
+// location();
 function hamburguerMenu () {
   navMenu.classList.toggle("nav-menu_visible");
   if (navMenu.classList.contains("nav-menu_visible")) {
@@ -46,7 +51,6 @@ let technology = document.querySelector("li .technology");
 
 let contact = document.querySelector("li .contact");
 
-const a = document.querySelector(".nav-menu a");
 
 
 
@@ -79,5 +83,33 @@ contact.addEventListener("click", () =>{
 });
 
 
+let width = home.offsetWidth;
+let width2 = cv.offsetWidth;
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.documentElement.scrollTop > width) {
+    portfolio.classList.add("nav-menu-link_active");
+    home.classList.remove("nav-menu-link_active");
+    technology.classList.remove("nav-menu-link_active");
+    contact.classList.remove("nav-menu-link_active");
+
+  } else if (document.documentElement.scrollTop < width) {
+    home.classList.add("nav-menu-link_active");
+
+    portfolio.classList.remove("nav-menu-link_active");
+    technology.classList.remove("nav-menu-link_active");
+    contact.classList.remove("nav-menu-link_active");
+    
+  }
+  if (document.documentElement.scrollTop > width2) {
+    technology.classList.add("nav-menu-link_active");
+
+    portfolio.classList.remove("nav-menu-link_active");
+    home.classList.remove("nav-menu-link_active");
+    contact.classList.remove("nav-menu-link_active");
+  } 
+};
 
 
