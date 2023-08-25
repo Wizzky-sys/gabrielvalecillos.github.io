2c98acfd5426b3760744a647be40dde4899db04a
Gabriel <alejandrovalecillosaular@gmail.com>
2023-08-25 14:22:53 -0400

    a

--- js/removepreloader.js
+++
@@ -1,7 +0,0 @@
-const removeLoader = () => {
-    var preload = document.querySelector (".pre");
-    preload.style.visibility = 'hidden';
-    preload.style.opacity = '0';
-  
-  };
-export { removeLoader };
